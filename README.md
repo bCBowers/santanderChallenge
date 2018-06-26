8b4b1f5f1c1bce827d24bdabbc140878ed29bae6
bCBowers <40600880+bCBowers@users.noreply.github.com>
2018-06-26 12:06:18 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# santanderChallenge
+This project is a solution to the Santander Value Prediction Challenge: https://www.kaggle.com/c/santander-value-prediction-challenge. I joined this challenge to expirement with ensemble models like LightGBM and XGBoost. My solution combines four different ensemble approaches using a simple averages. The challenge has only been live for a few days, so I plan to iterate and improve on my solution. As of 6/26, I'm in 392 place out of 1389 teams and I'm currently working alone.
+
+The four models applied so far are:
+
+- Feature Reduction and LightGBM
+
+- LightGBM
+
+- Feature Reduction/Clustering and Category Boosting Regression
+
+- Extreme Gradient Boosting
+
+As expected, an average of the four techniques produces better results than any technique individually.
+
+File Descriptions:
+
+santanderv1.py -> ensemble model code
+
+train.py, test.py, sample_submission.csv -> files provided by the Kaggle challenge
---
+++ satanderv1.py
@@ -0,0 +1,455 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 21 06:05:16 2018
+
+@author: M29480
+"""
+
+import pandas as pd
+import numpy as np
+from sklearn.cross_validation import KFold
+import lightgbm as lgb
+import time
+from sklearn import model_selection
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import TruncatedSVD
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import normalize
+import xgboost as xgb
+from sklearn.decomposition import FastICA, FactorAnalysis
+from sklearn.random_projection import GaussianRandomProjection, SparseRandomProjection
+from catboost import CatBoostRegressor
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# Read train and test files
+train_full = pd.read_csv('train.csv')
+test_full = pd.read_csv('test.csv')
+
+########### Log(target), drop duplicative/sparse columns, generate statistics, cluster, and Light Gradient Boosting
+
+train_df = train_full
+test_df = test_full
+
+X_train = train_df.drop(["ID", "target"], axis=1)
+y_train = np.log1p(train_df["target"].values)
+
+X_test = test_df.drop(["ID"], axis=1)
+
+# check and remove constant columns
+colsToRemove = []
+for col in X_train.columns:
+    if X_train[col].std() == 0: 
+        colsToRemove.append(col)
+        
+# remove constant columns in the training set
+X_train.drop(colsToRemove, axis=1, inplace=True)
+
+# remove constant columns in the test set
+X_test.drop(colsToRemove, axis=1, inplace=True) 
+
+# Check and remove duplicate columns
+colsToRemove = []
+colsScaned = []
+dupList = {}
+
+columns = X_train.columns
+
+for i in range(len(columns)-1):
+    v = X_train[columns[i]].values
+    dupCols = []
+    for j in range(i+1,len(columns)):
+        if np.array_equal(v, X_train[columns[j]].values):
+            colsToRemove.append(columns[j])
+            if columns[j] not in colsScaned:
+                dupCols.append(columns[j]) 
+                colsScaned.append(columns[j])
+                dupList[columns[i]] = dupCols
+                
+# remove duplicate columns in the training set
+X_train.drop(colsToRemove, axis=1, inplace=True) 
+
+# remove duplicate columns in the testing set
+X_test.drop(colsToRemove, axis=1, inplace=True)
+
+def drop_sparse(train, test):
+    flist = [x for x in train.columns if not x in ['ID','target']]
+    for f in flist:
+        if len(np.unique(train[f]))<2:
+            train.drop(f, axis=1, inplace=True)
+            test.drop(f, axis=1, inplace=True)
+    return train, test
+
+X_train, X_test = drop_sparse(X_train, X_test)
+
+
+def add_SumZeros(train, test, features):
+    flist = [x for x in train.columns if not x in ['ID','target']]
+    if 'SumZeros' in features:
+        train.insert(1, 'SumZeros', (train[flist] == 0).astype(int).sum(axis=1))
+        test.insert(1, 'SumZeros', (test[flist] == 0).astype(int).sum(axis=1))
+    flist = [x for x in train.columns if not x in ['ID','target']]
+
+    return train, test
+
+X_train, X_test = add_SumZeros(X_train, X_test, ['SumZeros'])
+
+
+
+def add_SumValues(train, test, features):
+    flist = [x for x in train.columns if not x in ['ID','target']]
+    if 'SumValues' in features:
+        train.insert(1, 'SumValues', (train[flist] != 0).astype(int).sum(axis=1))
+        test.insert(1, 'SumValues', (test[flist] != 0).astype(int).sum(axis=1))
+    flist = [x for x in train.columns if not x in ['ID','target']]
+
+    return train, test
+
+X_train, X_test = add_SumValues(X_train, X_test, ['SumValues'])
+
+def add_OtherAgg(train, test, features):
+    flist = [x for x in train.columns if not x in ['ID','target','SumZeros','SumValues']]
+    if 'OtherAgg' in features:
+        train['Mean']   = train[flist].mean(axis=1)
+        train['Median'] = train[flist].median(axis=1)
+        train['Mode']   = train[flist].mode(axis=1)
+        train['Max']    = train[flist].max(axis=1)
+        train['Var']    = train[flist].var(axis=1)
+        train['Std']    = train[flist].std(axis=1)
+        
+        test['Mean']   = test[flist].mean(axis=1)
+        test['Median'] = test[flist].median(axis=1)
+        test['Mode']   = test[flist].mode(axis=1)
+        test['Max']    = test[flist].max(axis=1)
+        test['Var']    = test[flist].var(axis=1)
+        test['Std']    = test[flist].std(axis=1)
+    flist = [x for x in train.columns if not x in ['ID','target','SumZeros','SumValues']]
+
+    return train, test
+
+X_train, X_test = add_OtherAgg(X_train, X_test, ['OtherAgg'])
+
+flist = [x for x in X_train.columns if not x in ['ID','target']]
+
+flist_kmeans = []
+for ncl in range(2,11):
+    cls = KMeans(n_clusters=ncl)
+    cls.fit_predict(X_train[flist].values)
+    X_train['kmeans_cluster_'+str(ncl)] = cls.predict(X_train[flist].values)
+    X_test['kmeans_cluster_'+str(ncl)] = cls.predict(X_test[flist].values)
+    flist_kmeans.append('kmeans_cluster_'+str(ncl))
+
+flist = [x for x in X_train.columns if not x in ['ID','target']]
+
+n_components = 20
+flist_pca = []
+pca = PCA(n_components=n_components)
+x_train_projected = pca.fit_transform(normalize(X_train[flist], axis=0))
+x_test_projected = pca.transform(normalize(X_test[flist], axis=0))
+for npca in range(0, n_components):
+    X_train.insert(1, 'PCA_'+str(npca+1), x_train_projected[:, npca])
+    X_test.insert(1, 'PCA_'+str(npca+1), x_test_projected[:, npca])
+    flist_pca.append('PCA_'+str(npca+1))
+
+def run_lgb(train_X, train_y, val_X, val_y, test_X):
+    params = {
+        "objective" : "regression",
+        "metric" : "rmse",
+        "num_leaves" : 30,
+        "learning_rate" : 0.01,
+        "bagging_fraction" : 0.7,
+        "feature_fraction" : 0.7,
+        "bagging_frequency" : 5,
+        "bagging_seed" : 2018,
+        "verbosity" : -1
+    }
+    
+    lgtrain = lgb.Dataset(train_X, label=train_y)
+    lgval = lgb.Dataset(val_X, label=val_y)
+    evals_result = {}
+    model = lgb.train(params, lgtrain, 1000, valid_sets=[lgtrain, lgval], early_stopping_rounds=100, 
+                      verbose_eval=200, evals_result=evals_result)
+    
+    pred_test_y = model.predict(test_X, num_iteration=model.best_iteration)
+    return pred_test_y, model, evals_result
+
+# Training LGB
+seeds = [42, 2018]
+pred_test_full_seed = 0
+for seed in seeds:
+    kf = model_selection.KFold(n_splits=5, shuffle=True, random_state=seed)
+    pred_test_full = 0
+    for dev_index, val_index in kf.split(X_train):
+        dev_X, val_X = X_train.loc[dev_index,:], X_train.loc[val_index,:]
+        dev_y, val_y = y_train[dev_index], y_train[val_index]
+        pred_test, model, evals_result = run_lgb(dev_X, dev_y, val_X, val_y, X_test)
+        pred_test_full += pred_test
+    pred_test_full /= 5.
+    pred_test_full = np.expm1(pred_test_full)
+    pred_test_full_seed += pred_test_full
+    print("Seed {} completed....".format(seed))
+pred_test_full_seed /= np.float(len(seeds))
+
+print("LightGBM Training Completed...")
+
+######### Log(target) and LightGBM
+
+train = train_full
+test = test_full
+
+#Data is highly left skewed so convert target to log(target)
+train['log_target']=np.log(1+train['target'])
+y=train['log_target']
+train.drop(['target','log_target'], axis=1, inplace=True)
+
+test_id=test['ID']
+train_id=train['ID']
+test.drop(['ID'], axis=1, inplace=True)
+train.drop(['ID'], axis=1, inplace=True)
+
+lgbm_params =  {
+    'task': 'train',
+    'boosting_type': 'gbdt',
+    'objective': 'regression',
+    'metric': 'rmse',
+    'max_depth': 8,
+    'num_leaves': 64,  # 63, 127, 255
+    'feature_fraction': 0.8, # 0.1, 0.01
+    'bagging_fraction': 0.8,
+    'learning_rate': 0.01, #0.00625,#125,#0.025,#05,
+    'verbosose': 1
+}
+
+Y_target = []
+for fold_id,(train_idx, val_idx) in enumerate(KFold(n=train.shape[0],n_folds=10, random_state=42,shuffle=True)):
+    print('FOLD:',fold_id)
+    X_train = train.values[train_idx]
+    y_train = y.values[train_idx]
+    X_valid = train.values[val_idx]
+    y_valid =  y.values[val_idx]
+    
+    
+    lgtrain = lgb.Dataset(X_train, y_train,
+                feature_name=train.columns.tolist(),
+    #             categorical_feature = categorical
+                         )
+
+    lgvalid = lgb.Dataset(X_valid, y_valid,
+                feature_name=train.columns.tolist(),
+    #             categorical_feature = categorical
+                         )
+
+    modelstart = time.time()
+    lgb_clf = lgb.train(
+        lgbm_params,
+        lgtrain,
+        num_boost_round=30000,
+        valid_sets=[lgtrain, lgvalid],
+        valid_names=['train','valid'],
+        early_stopping_rounds=100,
+        verbose_eval=100
+    )
+    
+    test_pred = lgb_clf.predict(test.values)
+    Y_target.append(np.exp(test_pred)-1)
+    print('fold finish after', time.time()-modelstart)
+    
+Y_target = np.array(Y_target)
+Y_target.shape
+
+X_target = []
+X_pred = lgb_clf.predict(train.values)
+X_target.append(np.exp(X_pred)-1)
+X_target = np.array(X_target)
+X_target.shape
+
+######## Log(target), cluster and categorize data, and run Category Boosting regression
+
+print("Load data...")
+train = train_full
+train_raw = train
+test = test_full
+subm = pd.read_csv('sample_submission.csv')
+print("Train shape: {}\nTest shape: {}".format(train.shape, test.shape))
+
+PERC_TRESHOLD = 0.98   ### Percentage of zeros in each feature ###
+N_COMP = 22            ### Number of decomposition components ###
+
+
+target = np.log1p(train['target']).values
+
+print("Define training features...")
+exclude_other = ['ID', 'target']
+train_features = []
+for c in train.columns:
+    if c not in exclude_other:
+        train_features.append(c)
+print("Number of featuress for training: %s" % len(train_features))
+
+train, test = train[train_features], test[train_features]
+print("\nTrain shape: {}\nTest shape: {}".format(train.shape, test.shape))
+
+
+print("\nStart decomposition process...")
+print("PCA")
+pca = PCA(n_components=N_COMP, random_state=17)
+pca_results_train = pca.fit_transform(train)
+pca_results_test = pca.transform(test)
+
+print("tSVD")
+tsvd = TruncatedSVD(n_components=N_COMP, random_state=17)
+tsvd_results_train = tsvd.fit_transform(train)
+tsvd_results_test = tsvd.transform(test)
+
+print("ICA")
+ica = FastICA(n_components=N_COMP, random_state=17)
+ica_results_train = ica.fit_transform(train)
+ica_results_test = ica.transform(test)
+
+print("FA")
+fa = FactorAnalysis(n_components=N_COMP, random_state=17)
+fa_results_train = fa.fit_transform(train)
+fa_results_test = fa.transform(test)
+
+print("GRP")
+grp = GaussianRandomProjection(n_components=N_COMP, eps=0.1, random_state=17)
+grp_results_train = grp.fit_transform(train)
+grp_results_test = grp.transform(test)
+
+print("SRP")
+srp = SparseRandomProjection(n_components=N_COMP, dense_output=True, random_state=17)
+srp_results_train = srp.fit_transform(train)
+srp_results_test = srp.transform(test)
+
+print("Append decomposition components to datasets...")
+for i in range(1, N_COMP + 1):
+    train['pca_' + str(i)] = pca_results_train[:, i - 1]
+    test['pca_' + str(i)] = pca_results_test[:, i - 1]
+    
+    train['tsvd_' + str(i)] = tsvd_results_train[:, i - 1]
+    test['tsvd_' + str(i)] = tsvd_results_test[:, i - 1]
+
+    train['ica_' + str(i)] = ica_results_train[:, i - 1]
+    test['ica_' + str(i)] = ica_results_test[:, i - 1]
+    
+    train['fa_' + str(i)] = fa_results_train[:, i - 1]
+    test['fa_' + str(i)] = fa_results_test[:, i - 1]
+
+    train['grp_' + str(i)] = grp_results_train[:, i - 1]
+    test['grp_' + str(i)] = grp_results_test[:, i - 1]
+
+    train['srp_' + str(i)] = srp_results_train[:, i - 1]
+    test['srp_' + str(i)] = srp_results_test[:, i - 1]
+print("\nTrain shape: {}\nTest shape: {}".format(train.shape, test.shape))
+
+
+print("\nModelling...")
+def rmsle(y_true, y_pred):
+    assert len(y_true) == len(y_pred)
+    return np.sqrt(np.mean(np.power(np.log(y_true + 1) - np.log(y_pred + 1), 2)))
+
+folds = KFold(n_splits=5, shuffle=True, random_state=546789)
+oof_preds = np.zeros(train.shape[0])
+sub_preds = np.zeros(test.shape[0])
+train_preds = np.zeros(train.shape[0])
+
+
+for n_fold, (trn_idx, val_idx) in enumerate(folds.split(train)):
+    trn_x, trn_y = train.ix[trn_idx], target[trn_idx]
+    val_x, val_y = train.ix[val_idx], target[val_idx]
+    
+    cb_model = CatBoostRegressor(iterations=1000,
+                                 learning_rate=0.1,
+                                 depth=4,
+                                 l2_leaf_reg=20,
+                                 bootstrap_type='Bernoulli',
+                                 subsample=0.6,
+                                 eval_metric='RMSE',
+                                 metric_period=50,
+                                 od_type='Iter',
+                                 od_wait=45,
+                                 random_seed=17,
+                                 allow_writing_files=False)
+    
+    cb_model.fit(trn_x, trn_y,
+                 eval_set=(val_x, val_y),
+                 cat_features=[],
+                 use_best_model=True,
+                 verbose=True)    
+    
+    oof_preds[val_idx] = cb_model.predict(val_x)
+    sub_preds += cb_model.predict(test) / folds.n_splits
+    
+    train_preds += cb_model.predict(train) / folds.n_splits
+    
+    print("Fold %2d RMSLE : %.6f" % (n_fold+1, rmsle(np.exp(val_y)-1, np.exp(oof_preds[val_idx])-1)))
+
+print("Full RMSLE score %.6f" % rmsle(np.exp(target)-1, np.exp(oof_preds)-1)) 
+
+######## Log(train) and then Extreme Gradient Boosting
+
+train_df = train_full
+test_df = test_full
+
+X_train = train_df.drop(["ID", "target"], axis=1)
+y_train = np.log1p(train_df["target"].values)
+
+X_test = test_df.drop(["ID"], axis=1)
+
+dev_X, val_X, dev_y, val_y = train_test_split(X_train, y_train, test_size = 0.2, random_state = 42)
+
+
+def run_xgb(train_X, train_y, val_X, val_y, test_X):
+    params = {'objective': 'reg:linear', 
+          'eval_metric': 'rmse',
+          'eta': 0.001,
+          'max_depth': 10, 
+          'subsample': 0.6, 
+          'colsample_bytree': 0.6,
+          'alpha':0.001,
+          'random_state': 42, 
+          'silent': True}
+    
+    tr_data = xgb.DMatrix(train_X, train_y)
+    va_data = xgb.DMatrix(val_X, val_y)
+    
+    watchlist = [(tr_data, 'train'), (va_data, 'valid')]
+    
+    model_xgb = xgb.train(params, tr_data, 10000, watchlist, maximize=False, early_stopping_rounds = 100, verbose_eval=50)
+    
+    dtest = xgb.DMatrix(test_X)
+    xgb_pred_y = np.expm1(model_xgb.predict(dtest, ntree_limit=model_xgb.best_ntree_limit))
+    
+    return xgb_pred_y, model_xgb
+
+# Training XGB
+pred_test_xgb, model_xgb = run_xgb(dev_X, dev_y, val_X, val_y, X_test)
+print("XGB Training Completed...")
+
+'''
+X1 = X_target.mean(axis=0)
+X2 = np.exp(train_preds)-1
+
+X=pd.DataFrame({'x1':X1, 'x2':X2})
+
+final_mod = sm.OLS(train_raw['target'], X).fit()
+
+final_mod.summary()
+
+sub = pd.read_csv('sample_submission.csv')
+
+Y=pd.DataFrame({'x1':Y_target.mean(axis=0), 'x2':np.exp(sub_preds)-1})
+
+final_pred = final_mod.predict(Y)
+sub['target'] = final_pred
+'''
+
+sub = pd.read_csv('sample_submission.csv')
+pred = (Y_target.mean(axis=0) + (np.exp(sub_preds)-1) + pred_test_xgb + pred_test_full_seed)/4
+
+sub['target'] = pred
+
+sub.to_csv('submission.csv', index=False)
